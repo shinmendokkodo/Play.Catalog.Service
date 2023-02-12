d55d99f62b22a6983fc1e928825cec9cc7f950fe
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2023-02-12 23:52:37 +0530

    Closes #2

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Play.Catalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController: ControllerBase
+    {
+
+    }
+}
